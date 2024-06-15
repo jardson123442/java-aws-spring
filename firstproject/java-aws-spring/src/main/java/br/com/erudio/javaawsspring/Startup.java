d212bbe8c3564b5da6391ad894bbd05d379644cb
JardsonVenancio <126803792+jardson123442@users.noreply.github.com>
2024-06-15 16:30:08 -0300

    Start Project

---
+++ firstproject/java-aws-spring/src/main/java/br/com/erudio/javaawsspring/Startup.java
@@ -0,0 +1,13 @@
+package br.com.erudio.javaawsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAwsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAwsSpringApplication.class, args);
+	}
+
+}
---
+++ firstproject/java-aws-spring/src/test/java/br/com/erudio/javaawsspring/JavaAwsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.javaawsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAwsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
